4f18f812844709c2b4e2c3a474891e3c554d88c6
3lv27 <elvin.gm27@gmail.com>
2017-11-07 09:06:00 +0100

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,66 @@
+'use strict';
+
+
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.direction = null;
+    this.MAXFLOOR = 10;
+
+    this.requests = [];
+    this.waitingList = [];
+    this.passengers = [];
+
+    this._intervalID = null;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this._intervalID = setInterval(this.update, 1000);
+  }
+  stop() {
+    clearInterval(this._intervalID);
+  }
+  update() {
+    this.log();
+  }
+  _passengersEnter(person) {
+    if (this.floor === this.requests) {
+      this.passengers.push(person.name);
+      this.waitingList.shift();
+      this.requests.push(person.destinationFloor);
+      console.log(`${person.name} has enter the elevator`);
+    }
+
+  }
+  _passengersLeave(person) {
+    if (person.destinationFloor === this.floor) {
+      let passenger = this.passengers.indexOf(person.name);
+      this.passengers.splice(passenger, 1);
+    }
+
+  }
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+      this.direction = 'UP';
+    }
+  }
+  floorDown() {
+    if (this.floor < 0) {
+      this.floor--;
+      this.direction = 'DOWN';
+    }
+  }
+  call(person) {
+
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person.name);
+
+  }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,18 @@
+'use strict';
+
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+const elevator = new Elevator();
+const person = new Person('Elvin', 2, 9);
+
+elevator.start();
+elevator.floorUp();
+elevator.floorUp();
+elevator.floorUp();
+
+
+elevator.log();
+elevator.call(person);
+elevator._passengersEnter(person);
